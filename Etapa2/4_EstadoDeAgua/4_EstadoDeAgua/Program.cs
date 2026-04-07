4bd17e341fd543b3fd825c969c6420a61e62ab0f
RAli42 <raed,doce@gmail.com>
2026-04-07 16:40:12 -0300

    benicio me la come

---
+++ Etapa2/4_EstadoDeAgua/4_EstadoDeAgua/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_EstadoDeAgua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la temperatura del agua: ");
+            int temp = int.Parse(Console.ReadLine());
+            if (temp >= 100)
+            {
+                Console.WriteLine("El Agua esta hirviendo.");
+            }
+            if ((temp < 100) && (temp > 0))
+            {
+                Console.WriteLine("El Agua esta en estado liquido.");
+            }
+            if (temp <= 0)
+            {
+                Console.WriteLine("El Agua esta congelada.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/5_Apocalipsis/5_Apocalipsis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Apocalipsis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            Console.WriteLine("Tiene un refugio? (si/no)");
+            if(Console.ReadLine().ToLower() == "si")
+            {
+                bool refu = true;
+            }
+            Console.WriteLine("Tiene comida? (true/false)");
+            bool comi = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tiene un bate? (true/false)");
+            bool bate = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tiene agua? (true/false)");
+            bool agua = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tiene una mochila? (true/false)");
+            bool mochila = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Tiene un botiquin? (true/false)");
+            bool boti = bool.Parse(Console.ReadLine());
+            if ((refu == true ) || (mochila == true))
+            {
+                contador = contador + 1;
+            }
+            if ((comi == true) || (agua == true))
+            {
+                contador = contador + 1;
+            }
+            if ((bate == true) || (boti == true))
+            {
+                contador = contador + 1;
+            }
+            if(contador >= 2)
+            {
+                Console.WriteLine("Sobreviviras al apocalipsis.");
+            }
+            if (contador <= 1)
+            {
+                Console.WriteLine("No sobreviviras al apocalipsis.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
